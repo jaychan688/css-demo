067a3bfe223dc024704e1f9590d98d12146d1b99
Jay Chan <jaychan688@gmail.com>
2021-07-25 09:15:28 +0800

    Initial Setup

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+// postcss 執行順序應該是由上往下
+const postcssConfig = {
+	plugins: [
+		require('postcss-import'),
+		require('postcss-css-variables'),
+		require('postcss-color-function'),
+		require('postcss-hexrgba'),
+		require('postcss-mixins'),
+		require('postcss-nested'),
+		require('postcss-preset-env'),
+	],
+}
+
+if (process.env.NODE_ENV === 'production')
+	postcssConfig.plugins.push(require('cssnano'))
+
+module.exports = postcssConfig
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const components = ['profile-john']
+
+const componentsLinks = components
+	.map(
+		components =>
+			`<li><a class="list__item" target="_blank" href="${components}/index.html">${components}</a></li>`
+	)
+	.join('')
+
+document
+	.querySelector('#root')
+	.insertAdjacentHTML(
+		'beforeend',
+		`<ul class="list list--green">${componentsLinks}</ul>`
+	)
